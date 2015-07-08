2be6b6843401216107e6cc6d49b88f9bde461646
erlangxk <simon.xikun@gmail.com>
2015-07-08 21:28:07 +0800

    change to use Seq instead of List

--- src/main/scala/coder/simon/scalaz/exercise/M1.scala
+++ src/main/scala/coder/simon/scalaz/exercise/M1.scala
@@ -1,27 +1,21 @@
 package coder.simon.scalaz.exercise
 
 object M1 {
-
-  val l = List(1, 2, 3, 5, 6, 7, 4, 5, 6, 7, 8, 9, 10, 11, 12, 3, 4)
-
-  def loop(l: List[Int], temp: List[Int], result: List[List[Int]]): List[List[Int]] = l match {
-    case h :: t => temp match {
-      case Nil      => loop(t, List(h), result)
-      case h2 :: t2 => if (h > h2) loop(t, h :: temp, result) else loop(t, List(h), temp :: result)
+  def loop(l: Seq[Int], temp: Seq[Int], result: Seq[Seq[Int]]): Seq[Seq[Int]] = l match {
+    case h +: t => temp match {
+      case Nil      => loop(t, Seq(h), result)
+      case h2 +: t2 => if (h > h2) loop(t, h +: temp, result) else loop(t, Seq(h), temp +: result)
     }
-    case Nil => temp :: result
+    case Nil => temp +: result
   }
 
-  def result(ls: List[List[Int]]) = {
-    ls.sortWith((l1, l2) => l1.size > l2.size).headOption.map(_.reverse)
-  }
+  def result(ls: Seq[Seq[Int]]) = ls.sortWith((l1, l2) => l1.size > l2.size).headOption.map(_.reverse)
 
   def main(args: Array[String]) = {
-    val x = loop(l, List.empty[Int], List.empty[List[Int]])
+    val l = Seq(1, 2, 3, 5, 6, 7, 4, 5, 6, 7, 8, 9, 10, 11, 12, 3, 4)
+    val x = loop(l, Seq.empty[Int], Seq.empty[Seq[Int]])
     println(x)
     val y = result(x)
     println(y)
-
   }
-
 }
