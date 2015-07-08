69706e2379c1f8f1fe60203f16f86cfdcef73b9a
erlangxk <simon.xikun@gmail.com>
2015-07-08 23:43:23 +0800

    remove the duplicated list when splits, and simplify the code

--- src/main/scala/coder/simon/scalaz/exercise/M2.scala
+++ src/main/scala/coder/simon/scalaz/exercise/M2.scala
@@ -11,17 +11,8 @@ object M2 {
   }
 
   def step(v: Int, r: Seq[Seq[Int]]): Seq[Seq[Int]] = {
-    var added = false
-    def add(r: Seq[Int]) = if (v >= r.head) {
-      added = true; Seq(v +: r)
-    } else {
-      val l = r.dropWhile(_ > v)
-      if (l.isEmpty) Seq(r) else {
-        added = true; Seq(v +: l, r)
-      }
-    }
-    val n = r flatMap add
-    if (added) n else Seq(v) +: r
+    def loop(l: Seq[Int]) = if (v >= l.head) Set(v +: l) else Set(v +: l.dropWhile(_ > v), l)
+    (r.toSet flatMap loop).toSeq
   }
 
   def max(l: Seq[Seq[Int]]) = l.sortWith { (l1, l2) => l1.size > l2.size }.head
@@ -30,7 +21,7 @@ object M2 {
     val l = Seq(1, 2, 3, 5, 6, 7, 4, 5, 6, 7, 8, 8, 8, 9, 10, 11, 12, 3, 4)
     test(l)
     println("xxxxxxxx")
-    val l2 = Seq(1, 2, 10, 4, 5)
+    val l2 = Seq(100, 101, 102, 1, 2, 10, 4, 5)
     test(l2)
   }
 
