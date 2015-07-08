2e9d21c0d02378d19208a63efea44e1f469cf3e2
erlangxk <simon.xikun@gmail.com>
2015-07-08 21:45:06 +0800

    if only want the length, then this is the newest version

--- src/main/scala/coder/simon/scalaz/exercise/M1.scala
+++ src/main/scala/coder/simon/scalaz/exercise/M1.scala
@@ -1,21 +1,20 @@
 package coder.simon.scalaz.exercise
 
 object M1 {
-  def loop(l: Seq[Int], temp: Seq[Int], result: Seq[Seq[Int]]): Seq[Seq[Int]] = l match {
+
+  def max(a: Int, b: Int) = if (a > b) a else b
+
+  def loop(l: Seq[Int], temp: Seq[Int], result: Int): Int = l match {
     case h +: t => temp match {
       case Nil      => loop(t, Seq(h), result)
-      case h2 +: t2 => if (h >= h2) loop(t, h +: temp, result) else loop(t, Seq(h), temp +: result)
+      case h2 +: t2 => if (h >= h2) loop(t, h +: temp, result) else loop(t, Seq(h), max(temp.size, result))
     }
-    case Nil => temp +: result
+    case Nil => max(temp.size, result)
   }
-
-  def result(ls: Seq[Seq[Int]]) = ls.sortWith((l1, l2) => l1.size > l2.size).headOption.map(_.reverse)
 
   def main(args: Array[String]) = {
     val l = Seq(1, 2, 3, 5, 6, 7, 4, 5, 6, 7, 8, 8, 8, 9, 10, 11, 12, 3, 4)
-    val x = loop(l, Seq.empty[Int], Seq.empty[Seq[Int]])
+    val x = loop(l, Seq.empty[Int], 0)
     println(x)
-    val y = result(x)
-    println(y)
   }
 }
