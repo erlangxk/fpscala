67ea1773b4387f718cbb307f4b26d450be397c3b
simon.sammi <simon.sammi@gmail.com>
2015-07-29 11:21:02 +0800

    phantom type example copied from scala-types-of-types

---
+++ src/main/scala/coder/simon/types/phantom/P1.scala
@@ -0,0 +1,31 @@
+package coder.simon.types.phantom
+
+/**
+ * @author simon
+ */
+object P1 {
+
+  sealed trait ServiceState
+  final class Started extends ServiceState
+  final class Stopped extends ServiceState
+
+  class Service[State <: ServiceState] private () {
+    def start[T >: State <: Stopped]() = this.asInstanceOf[Service[Started]]
+    def stop[T >: State <: Started]() = this.asInstanceOf[Service[Stopped]]
+  }
+
+  object Service {
+    def create() = new Service[Stopped]
+  }
+
+  def main(args: Array[String]): Unit = {
+    val x = Service.create()
+    x.start()
+
+    val y = Service.create()
+    // compile time error
+    // x.stop()
+
+  }
+
+}
