a38c87034060c557a124bf00e7b719e10bda9e23
simon.sammi <simon.sammi@gmail.com>
2015-08-12 18:31:39 +0800

    try unfold

--- src/main/scala/coder/simon/scalaz/exercise/M3.scala
+++ src/main/scala/coder/simon/scalaz/exercise/M3.scala
@@ -1,4 +1,6 @@
 package coder.simon.scalaz.exercise
+import scalaz._
+import Scalaz._
 
 object M3 {
 
@@ -14,6 +16,8 @@ object M3 {
     loop(n, Vector.empty[(Int, Int)])
   }
 
+  def gen2(n: (Int, Int)) = unfold(n) { case (x, y) => if (x > upperX || y > upperY) none else ((x, y), (x + 1, y + 1)).some }
+
   def main(args: Array[String]) {
     val l1 = for (i <- upperX to 0 by -1) yield (i, 0)
     val l2 = for (i <- 1 to upperY) yield (0, i)
@@ -22,6 +26,12 @@ object M3 {
     for (p <- path) {
       println(p)
     }
+
+    val path2 = l map gen2
+    for (p <- path2) {
+      println(p.toList)
+    }
+
   }
 
 }
