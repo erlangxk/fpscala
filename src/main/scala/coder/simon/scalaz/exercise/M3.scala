d46be00d3155ac20d9ea467f1617172dbdeeeb27
simon.sammi <simon.sammi@gmail.com>
2015-08-27 10:59:43 +0800

    refactor

--- src/main/scala/coder/simon/scalaz/exercise/M3.scala
+++ src/main/scala/coder/simon/scalaz/exercise/M3.scala
@@ -3,31 +3,29 @@ import scalaz._
 import Scalaz._
 
 object M3 {
-  val upperX = 3;
-  val upperY = 3;
 
-  def gen(n: (Int, Int)) = {
-    def loop(dot: (Int, Int), result: Vector[(Int, Int)]): Vector[(Int, Int)] = {
-      val (x, y) = dot
-      if (x <= upperX && y <= upperY) loop((x + 1, y + 1), result :+ dot)
-      else result
+  type Cords = Vector[(Int, Int)]
+  val acc = Vector.empty[(Int, Int)]
+
+  def gen2(maxX: Int, maxY: Int)(n: (Int, Int)) = {
+    def loop(x: Int, y: Int, result: Cords): Cords = {
+      if (x <= maxX && y <= maxY) loop(x + 1, y + 1, result :+ (x, y)) else result
     }
-    loop(n, Vector.empty[(Int, Int)])
+    loop(n._1, n._2, acc)
   }
 
-  def gen2(n: (Int, Int)) = unfold(n) { case (x, y) => if (x > upperX || y > upperY) none else ((x, y), (x + 1, y + 1)).some }
+  def gen1(maxX: Int, maxY: Int) = {
+    val l1 = for (i <- maxX to 0 by -1) yield (i, 0)
+    val l2 = for (i <- 1 to maxY) yield (0, i)
+    l1 ++ l2
+  }
 
   def main(args: Array[String]) {
-    val l1 = for (i <- upperX to 0 by -1) yield (i, 0)
-    val l2 = for (i <- 1 to upperY) yield (0, i)
-    val l = l1 ++ l2
-    val path = l map gen
+    val upperX = 3;
+    val upperY = 3;
+    val path = gen1(upperX, upperY) map gen2(upperX, upperY)
     for (p <- path) {
       println(p)
-    }
-    val path2 = l map gen2
-    for (p <- path2) {
-      println(p.toList)
     }
   }
 }
