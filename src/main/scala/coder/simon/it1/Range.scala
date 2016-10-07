6e2d4e4be8a91d05dd4410ddea0d48759c3ac8ed
erlangxk <simon.xikun@gmail.com>
2016-10-07 12:24:15 +0800

    refactor again for easy read

--- src/main/scala/coder/simon/it1/Range.scala
+++ src/main/scala/coder/simon/it1/Range.scala
@@ -1,20 +1,16 @@
 package coder.simon.it1
 
 object TaxRate {
-  case class Range(val min: Double = Double.MinValue, val max: Double = Double.MaxValue) {
+  case class Rate(val min: Double = Double.MinValue, val max: Double = Double.MaxValue, value: Double) {
     def include(number: Double): Boolean = number > min && number <= max
   }
 
-  private val range1 = Range(max = 2.0)
-  private val range2 = Range(min = 2.0, max = 5.0)
-  private val range3 = Range(min = 5.0)
-
   val rateTable = Map(
-    "Europe" -> Map(range1 -> 1.00, range2 -> 1.20, range3 -> 2.00),
-    "USA" -> Map(range1 -> 0.75, range2 -> 0.90, range3 -> 1.50),
-    "Japan" -> Map(range1 -> 0.70, range2 -> 0.80, range3 -> 1.35))
+    "Europe" -> List(Rate(max = 2.0, value = 1.00), Rate(min = 2.0, max = 5.0, value = 1.20), Rate(min = 5.0, value = 2.00)),
+    "USA" -> List(Rate(max = 2.0, value = 0.75), Rate(min = 2.0, max = 5.0, value = 0.90), Rate(min = 5.0, value = 1.50)),
+    "Japan" -> List(Rate(max = 2.0, value = 0.70), Rate(min = 2.0, max = 5.0, value = 0.80), Rate(min = 5.0, value = 1.35)))
 
-  def calcRate(from: String, capacity: Double) = for (m <- rateTable.get(from); p <- m.find(_._1.include(capacity))) yield p._2
+  def calcRate(from: String, capacity: Double) = for (m <- rateTable.get(from); p <- m.find(_.include(capacity))) yield p.value
 }
 
 object Calculator {
